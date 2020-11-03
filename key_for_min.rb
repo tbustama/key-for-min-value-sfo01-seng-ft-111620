1b1db0b007de1ceeef360e26b07d1841ec14de81
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-03 17:50:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest = name_hash[0].value
+  small_key = ""
+  name_hash.each do |key, value|
+    if value < smallest
+      smallest = value
+      small_key = key
+    end
+  end
+  return small_key
 end
