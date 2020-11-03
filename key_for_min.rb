df89d2f517cc08a1316d5bccb17004831e0602a4
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-03 18:00:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,16 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil
   else
-    smallest = 
+    smallest = []
     small_key = ""
     name_hash.each do |key, value|
-      if value. value < smallest
-        smallest = value
-        small_key = key
+      if smallest.empty?
+        smallest[0] = value 
+      else 
+        if value < smallest
+          smallest[0] = value
+          small_key = key
+        end
       end
     end
     return small_key
